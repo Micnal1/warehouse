9905e97a2c5ee9f98087b99106a709a98021fdbb
Michał Zając <diuf95o@gmail.com>
2021-02-05 16:03:06 +0100

    Add exit

---
+++ warehouse.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == '__main__':
+    while True:
+        option=input\
+            ("""Menu:\nexit\nWhat are you want to do?:""")
+
+        if option == 'exit':
+            break
+
+    input("exiting... see you! (press Enter)")
